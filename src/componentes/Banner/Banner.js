ce7a397c56286bd9f24ca629d4319fa3dfd44931
Jackson Paulo <jacksonpaulojack@hotmail.com>
2022-12-10 21:14:31 -0300

    primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <Fragment>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O Banner principal do Organo" />
+            </header>
+        </Fragment>
+    )
+}
+
+
+export default Banner 
